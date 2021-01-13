213f55a1c9ab7e998fd6a449e2e8ca67aba0af2a
amaslovskiy <maslovskiy@mail.ru>
2021-01-13 21:46:12 +0300

    Initial commit

---
+++ src/main/java/ru/ramzet89/dictionaryconsoleclient/DictionaryConsoleClientApplication.java
@@ -0,0 +1,13 @@
+package ru.ramzet89.dictionaryconsoleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryConsoleClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryConsoleClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ramzet89/dictionaryconsoleclient/DictionaryConsoleClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ramzet89.dictionaryconsoleclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryConsoleClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
